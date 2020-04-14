ffcba2623dc9529f900b995b089a68adb50fee6e
Roy Nyaga <nyagaandreroy@gmail.com>
2020-04-14 02:52:41 +0100

    add admin, config,  fbAuth and validators in util folder

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyBT59xM6GJ50aRHQvWAfW_rDkVqSAIxBg8',
+  authDomain: 'socialape-f403e.firebaseapp.com',
+  databaseURL: 'https://socialape-f403e.firebaseio.com',
+  projectId: 'socialape-f403e',
+  storageBucket: 'socialape-f403e.appspot.com',
+  messagingSenderId: '722445146634',
+  appId: '1:722445146634:web:247e7e68cd5fe4d18c9df3'
+}
